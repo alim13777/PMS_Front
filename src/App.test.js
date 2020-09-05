d57ddcc4495203fbfd78096cda55405cfdf5f7d2
Mojtaba <mfazelinia@gmail.com>
2020-09-05 14:25:44 +0430

    delete unused files

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import AppTest from './AppTest';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<AppTest />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
